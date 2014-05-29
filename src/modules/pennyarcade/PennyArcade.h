8e9b54cafc633e782da7a8483accc7cfb6a9dcb3
Josaphat Valdivia <josaphat.valdivia@gmail.com>
2014-05-29 05:27:35 -0400

    Add the framework according to issue #8 and include a stub of Penny Arcade plugin for issue #4

---
+++ src/modules/pennyarcade/PennyArcade.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "../../Module.h"
+
+class Channel;
+
+class PennyArcade :
+	public Module
+{
+public:
+	PennyArcade (const std::string & name);
+	virtual ~PennyArcade ();
+
+public: // Override
+	virtual void accept (Channel * src, const gloox::Message & msg);
+};
